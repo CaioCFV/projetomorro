79a2772f8b61cf2d2246244253f11a0f9d9b6546
Caio <caio2011dhd@gmail.com>
2025-07-20 15:52:20 -0300

    removendo variaveis

--- frontend/src/pages/Aluno/index.js
+++ frontend/src/pages/Aluno/index.js
@@ -1,48 +1,8 @@
 import { useEffect, useState } from "react";
 import { Heading1 } from "../../components/Typograph";
-import { InputField } from "../../components/Input";
 import { ServiceAluno } from "../../service/api";
-import { Button } from "../../components/Button";
-import Modal from "../../components/Modal";
 import CadastroAluno from "./components/CadastroAluno";
 import TableAlunos from "./components/TableAlunos";
-import ArrowForward from "@mui/icons-material/ArrowForward";
-// import Box from "@mui/material/Box";
-// import Typography from "@mui/material/Typography";
-// // import CustomizedDataGrid from "../../components/CustomizedDataGrid";
-
-// import Button from "@mui/material/Button";
-// import Grid from "@mui/material/Grid";
-// import TextField from "@mui/material/TextField";
-// import Modal from "@mui/material/Modal";
-// import Fade from "@mui/material/Fade";
-// import { fabClasses } from "@mui/material/Fab";
-// const columns = [
-//   {
-//     field: "nome_completo",
-//     headerName: "Nome",
-//     flex: 0.5,
-//     minWidth: 80,
-//   },
-//   {
-//     field: "cpf",
-//     headerName: "CPF",
-//     flex: 0.5,
-//     minWidth: 80,
-//   },
-//   {
-//     field: "celular",
-//     headerName: "Celular",
-//     flex: 0.5,
-//     minWidth: 80,
-//   },
-//   {
-//     field: "renda_familiar",
-//     headerName: "Renda",
-//     flex: 0.5,
-//     minWidth: 80,
-//   },
-// ];
 
 export default function Aluno() {
   const [data, setData] = useState([]);
